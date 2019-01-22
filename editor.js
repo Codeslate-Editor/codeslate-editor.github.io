4d73ccb4461e8ab8efc5367bb065b3d17efc0528
James-Livesey <jamesliveg@gmail.com>
2019-01-22 13:06:56 +0000

    Set language to HTML

--- editor.js
+++ editor.js
@@ -8,14 +8,15 @@ function newEditor() {
     editors.push({
         element: $(".editors").children().last(),
         cm: CodeMirror($(".editors").children().last()[0], {
-            value: `console.log("Hello, world!");`,
-            mode: "javascript",
+            value: `<h1>Hi!</h1>`,
+            mode: "htmlmixed",
             lineNumbers: true,
             matchBrackets: true,
             fixedGutter: true,
             tabsize: 4,
             indentUnit: 4,
             autoCloseBrackets: true,
+            autoCloseTags: true,
             extraKeys: {
                 "Tab": function(cm) {
                     cm.replaceSelection("    ", "end");
