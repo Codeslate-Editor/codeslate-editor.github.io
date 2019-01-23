991e1bcc3a8bfb344c29262cab29439093aec2dc
James-Livesey <jamesliveg@gmail.com>
2019-01-23 12:53:09 +0000

    Add better autocomplete

--- editor.js
+++ editor.js
@@ -8,7 +8,14 @@ function newEditor() {
     editors.push({
         element: $(".editors").children().last(),
         cm: CodeMirror($(".editors").children().last()[0], {
-            value: `<h1>Hi!</h1>`,
+            value: `<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+    </body>
+</html>`,
             mode: "htmlmixed",
             lineNumbers: true,
             matchBrackets: true,
@@ -26,8 +33,22 @@ function newEditor() {
         })
     });
 
-    editors[editors.length - 1].cm.on("keyup", function(cm, event) {
-        if (!cm.state.completionActive && event.keyCode != 13) {
+    editors[editors.length - 1].cm.on("keydown", function(cm, event) {
+        var blockedKeyCodes = [8, 9, 13, 16, 27, 32, 37, 38, 39, 40, 186, 190];
+        var blockedShiftKeyCodes = [190];
+
+        console.log(event.keyCode);
+        console.log(event.keyCode in blockedKeyCodes);
+        console.log(!event.shiftKey, !(blockedShiftKeyCodes.indexOf(event.keyCode) > -1));
+
+        if (
+            !cm.state.completionActive &&
+            !(blockedKeyCodes.indexOf(event.keyCode) > -1) &&
+            (
+                !event.shiftKey ||
+                !(blockedShiftKeyCodes.indexOf(event.keyCode) > -1)
+            )
+        ) {
             CodeMirror.commands.autocomplete(cm, null, {completeSingle: false});
         }
     });
