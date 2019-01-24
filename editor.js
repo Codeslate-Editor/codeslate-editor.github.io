0611ea93f990c24437ffb1ebd54f87a97733f58e
James-Livesey <jamesliveg@gmail.com>
2019-01-24 19:48:04 +0000

    Add directory support

--- editor.js
+++ editor.js
@@ -17,41 +17,99 @@ function newEditor(filename) {
     });
 
     currentEditor = editors.length - 1;
+    var fileStructure = filename.split("/");
+    var hierachyPosition = $("[data-folder='" + fileStructure.slice(0, fileStructure.length - 1).join("/") + "']");
 
-    $(".files").html($(".files").html() + `
+    // Create any non-existent directories
+
+    for (var f = 0; f < fileStructure.length; f++) {
+        var exists = 0;
+
+        for (var i = 0; i < editors.length; i++) {
+            if (editors[i]["filename"].split("/").slice(0, editors[i]["filename"].split("/").length - 1).join("/") + "#folder" == fileStructure.slice(0, fileStructure.length - 1).join("/") + "#folder") {
+                exists++;
+            }
+        }
+
+        if (exists < 2) {
+            var exists = false;
+
+            for (var i = 0; i < editors.length; i++) {
+                if (editors[i]["filename"] == fileStructure.slice(0, f).join("/") + "#folder") {
+                    exists = true;
+                }
+            }
+            
+            if (!exists) {
+                editors.push({
+                    filename: fileStructure.slice(0, f).join("/") + "#folder"
+                });
+
+                if (fileStructure.slice(0, f).join("/") != "") {
+                    $("[data-folder='" + fileStructure.slice(0, f - 1).join("/") + "']").html($("[data-folder='" + fileStructure.slice(0, f - 1).join("/") + "']").html() + `
+                        <div class="folder sidebarItem" data-name="#` + fileStructure[fileStructure.slice(0, f).length - 1] + `" data-folder="` + fileStructure.slice(0, f).join("/") + `">
+                            <div class="folderLabel" data-name="#"><i>folder</i> ` + fileStructure[fileStructure.slice(0, f).length - 1] + `</div>
+                        </div>
+                    `);
+
+                    if (f == fileStructure.length - 1) {
+                        newEditor(filename);
+                    }
+                }
+            }
+        }
+    }
+
+    // Create a file button
+
+    hierachyPosition.html(hierachyPosition.html() + `
         <button class="file sidebarItem" onclick="selectEditor(` + (editors.length - 1) + `);" data-editor-id="` + (editors.length - 1) + `"></button>
     `);
 
-    $(".files").children().last().text(filename).attr("data-name", filename);
+    hierachyPosition.find("button.file:contains('#init')").remove();
+    hierachyPosition.find("button.file").last().text(fileStructure[fileStructure.length - 1]).attr("data-name", filename);
 
-    var orderedDivs = $(".files").children().sort(function(first, second) {
-        return String.prototype.localeCompare.call($(first).attr("data-name").toLowerCase(), $(second).attr("data-name").toLowerCase());
-    });
+    // Sort out order of files
 
-    $(".files").empty().append(orderedDivs);
+    for (var i = 0; i < editors.length; i++) {
+        if (editors[i].filename.endsWith("#folder")) {
+            var hierachyPosition = $("[data-folder='" + editors[i].filename.replace("#folder", "") + "']");
+
+            var orderedDivs = hierachyPosition.children().sort(function(first, second) {
+                return String.prototype.localeCompare.call($(first).attr("data-name").toLowerCase(), $(second).attr("data-name").toLowerCase());
+            });
+
+            hierachyPosition.empty().append(orderedDivs);
+        }
+    }
+
+    // Select the correct editor
 
     selectEditor(editors.length - 1);
 }
 
 function selectEditor(editorID) {
-    currentEditor = editorID;
+    if (!editors[editorID].filename.endsWith("#folder")) {
+        currentEditor = editorID;
 
-    cm.setValue(editors[editorID].content);
+        cm.setValue(editors[editorID].content);
 
-    cm.setOption("mode", null);
+        cm.setOption("mode", null);
 
-    for (var i = 0; i < fileTypes.length; i++) {
-        if (editors[editorID].filename.endsWith(fileTypes[i].ending)) {
-            cm.setOption("mode", fileTypes[i].mode);
+        for (var i = 0; i < fileTypes.length; i++) {
+            if (editors[editorID].filename.endsWith(fileTypes[i].ending)) {
+                cm.setOption("mode", fileTypes[i].mode);
+            }
         }
-    }
 
-    $(".file").removeClass("selected");
-    $(".file[data-editor-id='" + editorID + "']").addClass("selected");
+        $(".file").removeClass("selected");
+        $(".file[data-editor-id='" + editorID + "']").addClass("selected");
+    }
 }
 
 function promptNewFile() {
     $(".newFilename").show().focus();
+    $(".newFilenameHint").show();
 }
 
 $(function() {
@@ -96,6 +154,7 @@ $(function() {
 
     updateSyntaxHighlighting();
 
+    newEditor("#init");
     newEditor("index.html");
 
     $(".newFilename").keydown(function(event) {
@@ -109,10 +168,16 @@ $(function() {
                     }
                 }
 
-                if (!exists && /^[0-9a-zA-Z ... ]+$/.test($(".newFilename").val().trim())) {
+                if (
+                    !exists &&
+                    /^[0-9a-zA-Z ... ]+$/.test($(".newFilename").val().trim().replace(/\//g, "")) &&
+                    !$(".newFilename").val().trim().startsWith("/") &&
+                    !/\/\//g.test($(".newFilename").val().trim().replace(/  /g, "").replace(/ \//g, "/"))
+                ) {
                     newEditor($(".newFilename").val().trim());
 
                     $(".newFilename").val("").hide();
+                    $(".newFilenameHint").hide();
                 }
             }
         }
@@ -120,6 +185,7 @@ $(function() {
 
     $(document).on("click", "*", function(event) {
         $(".newFilename").val("").hide();
+        $(".newFilenameHint").hide();
     });
 
     $(".newFilenameQueue, .newFilename").click(function(event) {
